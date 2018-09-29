a87c6fe865dd129b0e40b175bcf6ae0eafe4717d
usaka rokujou <usaka_rokujou@outlook.com>
2018-09-29 13:05:24 -0500

    ay vamos...

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  connect(){
+   
+  }
 }
